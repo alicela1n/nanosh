c82ee51dd8efe98e7920bae7cb13cfabdf562b58
Alice S <quarkyalice@disroot.org>
2025-07-20 10:29:20 -0700

    .clang-format: Add, and reformat of code

--- src/sh.c
+++ src/sh.c
@@ -1,47 +1,45 @@
+#include <errno.h>
+#include <readline/history.h>
+#include <readline/readline.h>
+#include <spawn.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include <string.h>
-#include <spawn.h>
-#include <unistd.h>
-#include <errno.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <readline/readline.h>
-#include <readline/history.h>
+#include <unistd.h>
 
 #include "include/str.h"
 
 struct cmd {
-    char **buffer;
-    unsigned int length;
+	char **buffer;
+	unsigned int length;
 };
 
-void dummy_handler(int dummy) {
-	return;
-}
+void dummy_handler(int dummy) { return; }
 
 int sh_built_in(struct cmd input, char **env) {
-    // Exit function
-    if (strcmp(input.buffer[0], "exit") == 0) {
-	exit(0);
-    }
-
-    // CD into a directory
-    if (strcmp(input.buffer[0], "cd") == 0) {
-	if (input.buffer[1] == NULL) {
-	    char *home = getenv("HOME");
-	    chdir(home);
-	    return 0;
-	} else {
-	    if (chdir(input.buffer[1]) == 0) {
-		return 0;
-	    } else {
-		printf("cd: no such directory\n");
-		return 1;
-	    }
+	// Exit function
+	if (strcmp(input.buffer[0], "exit") == 0) {
+		exit(0);
 	}
-    }
+
+	// CD into a directory
+	if (strcmp(input.buffer[0], "cd") == 0) {
+		if (input.buffer[1] == NULL) {
+			char *home = getenv("HOME");
+			chdir(home);
+			return 0;
+		} else {
+			if (chdir(input.buffer[1]) == 0) {
+				return 0;
+			} else {
+				printf("cd: no such directory\n");
+				return 1;
+			}
+		}
+	}
 
 	// history
 	if (strcmp(input.buffer[0], "history") == 0) {
@@ -62,101 +60,101 @@ int sh_built_in(struct cmd input, char **env) {
 		return 0;
 	}
 
-    // printenv
-    if (strcmp(input.buffer[0], "printenv") == 0) {
-	for (; *env; ++env) {
-	    printf("%s\n", *env);
+	// printenv
+	if (strcmp(input.buffer[0], "printenv") == 0) {
+		for (; *env; ++env) {
+			printf("%s\n", *env);
+		}
+		return 0;
 	}
-	return 0;
-    }
 
-    // setenv
-    if (strcmp(input.buffer[0], "setenv") == 0) {
-	if (input.buffer[1] == NULL || input.buffer[2] == NULL) {
-	    printf("No environment variable to be set\n");
-	    return 1;
-	} else {
-	    setenv(input.buffer[1], input.buffer[2], true);
-	    return 0;
+	// setenv
+	if (strcmp(input.buffer[0], "setenv") == 0) {
+		if (input.buffer[1] == NULL || input.buffer[2] == NULL) {
+			printf("No environment variable to be set\n");
+			return 1;
+		} else {
+			setenv(input.buffer[1], input.buffer[2], true);
+			return 0;
+		}
 	}
-    }
 
-    // help
-    if(strcmp(input.buffer[0], "help") == 0) {
-	printf("HELP: \n");
-	printf("\tbuiltin commands - \n");
-	printf("\tsetenv (Sets environment variables) \n");
-	printf("\tprintenv (Prints out the environment variables) \n");
-	printf("\tcd (Enters a directory) \n");
-	printf("\texit (Exits the shell) \n");
-	return 0;
-    }
-    return -1;
+	// help
+	if (strcmp(input.buffer[0], "help") == 0) {
+		printf("HELP: \n");
+		printf("\tbuiltin commands - \n");
+		printf("\tsetenv (Sets environment variables) \n");
+		printf("\tprintenv (Prints out the environment variables) \n");
+		printf("\tcd (Enters a directory) \n");
+		printf("\texit (Exits the shell) \n");
+		return 0;
+	}
+	return -1;
 }
 
 // sh_exec(): Execute a command
 int sh_exec(struct cmd input, char **env) {
-    int status = 0;
+	int status = 0;
 
-    pid_t pid;
-    pid_t wait;
+	pid_t pid;
+	pid_t wait;
 
-    if (input.buffer[0] == NULL)
+	if (input.buffer[0] == NULL)
+		return status;
+
+	status = sh_built_in(input, env);
+	if (status != -1)
+		return status;
+
+	status = posix_spawnp(&pid, input.buffer[0], NULL, NULL, input.buffer, env);
+	waitpid(pid, &status, 0);
+
 	return status;
-
-    status = sh_built_in(input, env);
-    if (status != -1)
-	return status;
-
-    status = posix_spawnp(&pid, input.buffer[0], NULL, NULL, input.buffer, env);
-    waitpid(pid, &status, 0);
-
-    return status;
 }
 
 // sh_loop(): The actual shell loop
 void sh_loop(int argc, char **argv, char **envp) {
-    char *line = NULL;
-    char *prompt = "> ";
+	char *line = NULL;
+	char *prompt = "> ";
 	struct cmd command;
-    char **env = envp;
-    int status = 0;
+	char **env = envp;
+	int status = 0;
 
-    while (true) {
-	signal(SIGINT, dummy_handler);
+	while (true) {
+		signal(SIGINT, dummy_handler);
 
-	line = readline(prompt);
+		line = readline(prompt);
 
-	if (!line)
-	    break;
+		if (!line)
+			break;
 
-	// Remove the new line
-	remove_newline(line);
+		// Remove the new line
+		remove_newline(line);
 
-	// Add command to history
-	add_history(line);
+		// Add command to history
+		add_history(line);
 
-	const unsigned int command_size = calculate_size_of_split_string(line, " ");
-	command.buffer = malloc(command_size * sizeof(char *));
+		const unsigned int command_size = calculate_size_of_split_string(line, " ");
+		command.buffer = malloc(command_size * sizeof(char *));
 
-	// Split the line into an array with the command and it's arguments
-	command.length = split_string(line, command.buffer, " ", command_size);
+		// Split the line into an array with the command and it's arguments
+		command.length = split_string(line, command.buffer, " ", command_size);
 
-	free(line);
+		free(line);
 
-	status = sh_exec(command, env);
-	printf("%i ", status);
+		status = sh_exec(command, env);
+		printf("%i ", status);
 
-	// Free the pointers in the command array
-	free_array_of_strings(command.buffer, command.length);
-    }
+		// Free the pointers in the command array
+		free_array_of_strings(command.buffer, command.length);
+	}
 
-    // Clean Up
-    free(command.buffer);
+	// Clean Up
+	free(command.buffer);
 }
 
 int main(int argc, char **argv, char **envp) {
 	using_history();
-    sh_loop(argc, argv, envp);
-    return EXIT_SUCCESS;
+	sh_loop(argc, argv, envp);
+	return EXIT_SUCCESS;
 }
--- src/str.c
+++ src/str.c
@@ -1,55 +1,55 @@
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stddef.h>
 #include <string.h>
 
 // split_string(): Take a string, array of pointers, delimiter, and max length as input,
 //                 split the string based on the delimiter and return the number
 //                 of strings in the array as output.
 unsigned int split_string(char *input, char **output, const char *delimiter, const unsigned int max_length) {
-    char *saveptr;
-    char *token = strtok_r(input, delimiter, &saveptr);
-    unsigned int length = 0;
+	char *saveptr;
+	char *token = strtok_r(input, delimiter, &saveptr);
+	unsigned int length = 0;
 
-    while (token != NULL) {
-        if (length == max_length)
-            exit(0);
+	while (token != NULL) {
+		if (length == max_length)
+			exit(0);
 
-        *output = strdup(token);
+		*output = strdup(token);
 
-        if (*output == NULL)
-            exit(0);
+		if (*output == NULL)
+			exit(0);
 
-        token = strtok_r(NULL, delimiter, &saveptr); 
-        length++;
-        output++;
-    }
-    *output = NULL;
+		token = strtok_r(NULL, delimiter, &saveptr);
+		length++;
+		output++;
+	}
+	*output = NULL;
 
-    return length;
+	return length;
 }
 
 // calculate_size_of_split_string(): Just calculate the size of the array for the split string
 unsigned int calculate_size_of_split_string(char *input, const char *delimiter) {
-    char *saveptr;
-    char *token = strtok_r(input, delimiter, &saveptr);
-    unsigned int length = 0;
+	char *saveptr;
+	char *token = strtok_r(input, delimiter, &saveptr);
+	unsigned int length = 0;
 
-    while (token != NULL) {
-        token = strtok_r(NULL, delimiter, &saveptr);
-        length++;
-    }
+	while (token != NULL) {
+		token = strtok_r(NULL, delimiter, &saveptr);
+		length++;
+	}
 }
 
 // remove_newline(): Remove a new line at the end of a string
 void remove_newline(char *input) {
-    if (input[strlen(input) - 1] == '\n')
-	input[strlen(input) - 1] = '\0';
+	if (input[strlen(input) - 1] == '\n')
+		input[strlen(input) - 1] = '\0';
 }
 
 // free_array_of_strings(): Frees an array of strings
 void free_array_of_strings(char **input, unsigned int input_length) {
-    for (int i = 0; i < input_length; i++) {
-	free(input[i]);
-    }
+	for (int i = 0; i < input_length; i++) {
+		free(input[i]);
+	}
 }
